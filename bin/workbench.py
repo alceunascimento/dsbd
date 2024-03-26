6793cd8a7c24e01388ecc1d4b9f663357381da08
aenascimento <alceuenascimento@gmail.com>
2024-03-26 20:07:37 -0300

    update

---
+++ bin/workbench.py
@@ -0,0 +1,5 @@
+
+keys = {'matricula','codigo','disciplina','periodo','ano','curriculo','ch','nota','frequencia','status','tipo','observacao','natureza','situacaoDiscente','nomeTuma','codigoCurriculoSie'}
+
+data = dict([(key,[]) for key in keys])
+data
