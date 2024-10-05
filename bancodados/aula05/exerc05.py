76a223b12730843763d25d673b5059c221d7a741
alceunascimento <alceuenascimento@gmail.com>
2024-10-04 21:03:33 -0300

    update

---
+++ bancodados/aula05/exerc05.py
@@ -0,0 +1,23 @@
+# Exercícios
+# Considerando que voce tem o banco de dados `dsbd05.db` 
+# Considerando o arquivo `microdados_ed_basica_2022_amostra_1000` possui 1.000 linhas, você deve fazer um programa Python (ou vários programas) que:
+# 1. Preenche o banco de dados com as escolas.
+- 'codigo'
+- 'nome'
+- 'cod_municipio'
+- 'endereco'
+- 'compl_endereco'
+- 'bairro'
+- 'cod-loc_dif'
+# 2. Preenche o banco de dados com os telefones das escolas.
+
+# 3. Preenche o banco de dados com alguns dos vínculos válidos.
+- nova tabela 'vinculos' com:
+    - 'codigo' = 1,2,3
+    - 'nome' = 
+    - 'descricao'
+## a. Para esse exercício, verifique no dicionário de dados, e cadastre pelo menos 3 vínculos manualmente (como fizemos em aula).
+# 4. Preenche o banco de dados com pelo menos medições 5 à sua escolha de cada escola.
+## a. Exemplo: para cada escola, serão cadastrados o número de alunos no ensino médio, se a escola possui água potável, …
+
+
